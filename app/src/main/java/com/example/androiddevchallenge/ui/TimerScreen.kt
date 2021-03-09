c6b6b7b20bec0b33bd5e6cd44b909f0ce7e067c8
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-09 23:57:01 +0900

    Refactor

--- app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
@@ -1,6 +1,5 @@
 package com.example.androiddevchallenge.ui
 
-import android.util.Log
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -21,26 +20,25 @@ import com.example.androiddevchallenge.ui.theme.purple200
 @Composable
 fun TimerScreen() {
     val viewModel = viewModel<TimerViewModel>()
-    StateLessTimerScreen(
-        duration = viewModel.countedTimeSecs,
-        timerStarted = viewModel.timerStarted
+    val sweepAngle: Float by animateFloatAsState(
+        targetValue = if (viewModel.timerStarted) 360f else 0f,
+        animationSpec = tween(
+            durationMillis = viewModel.countedTimeSecs * 1000,
+            easing = LinearEasing
+        )
     )
+    StateLessTimerScreen(sweepAngle = sweepAngle)
 }
 
 @Composable
-fun StateLessTimerScreen(duration: Int, timerStarted: Boolean) {
-    val angle: Float by animateFloatAsState(
-        targetValue = if (timerStarted) 360f else 0f,
-        animationSpec = tween(durationMillis = duration * 1000, easing = LinearEasing)
-    )
-    Log.d("tag", "duration: $duration, timerStarted: $timerStarted, angle: $angle")
+fun StateLessTimerScreen(sweepAngle: Float) {
     Canvas(modifier = Modifier.fillMaxSize()) {
         val arcSizePx = 300.dp.toPx()
         translate(size.width / 2, size.height / 2) {
             drawArc(
                 color = purple200,
                 startAngle = -90f,
-                sweepAngle = angle,
+                sweepAngle = sweepAngle,
                 useCenter = false,
                 style = Stroke(width = 10f, cap = StrokeCap.Round),
                 size = Size(arcSizePx, arcSizePx),
