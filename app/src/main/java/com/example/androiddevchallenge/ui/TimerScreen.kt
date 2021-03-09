25c1a66cf2af54b9eafeba2974a219ae96f1936d
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-09 22:57:55 +0900

    Show countdowned time

--- app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
@@ -1,8 +1,11 @@
 package com.example.androiddevchallenge.ui
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -10,17 +13,25 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.ui.theme.purple200
 
 @Composable
 fun TimerScreen() {
+    val sweepAngle = viewModel<TimerViewModel>().timerSweepAngle.collectAsState()
+    StateLessTimerScreen(sweepAngle = sweepAngle.value)
+}
+
+@Composable
+fun StateLessTimerScreen(sweepAngle: Float) {
+    val angle: Float by animateFloatAsState(targetValue = sweepAngle)
     Canvas(modifier = Modifier.fillMaxSize()) {
         val arcSizePx = 300.dp.toPx()
         translate(size.width / 2, size.height / 2) {
             drawArc(
                 color = purple200,
                 startAngle = 0f,
-                sweepAngle = 360f,
+                sweepAngle = angle,
                 useCenter = false,
                 style = Stroke(width = 10f, cap = StrokeCap.Round),
                 size = Size(arcSizePx, arcSizePx),
--- app/src/main/java/com/example/androiddevchallenge/ui/TimerViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/TimerViewModel.kt
@@ -2,21 +2,23 @@ package com.example.androiddevchallenge.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
-
 
 class TimerViewModel : ViewModel() {
 
+    private val maxTime = 50
+
     private val countDownTimeFlow: Flow<Int> = flow {
-        var count = 0
-        while (true) {
-            emit(count)
-            count++
-            kotlinx.coroutines.delay(1000L)
+        var seconds = 0
+        while (seconds < maxTime) {
+            emit(seconds)
+            seconds++
+            delay(1000L)
         }
     }
 
-    val timerSweepAngle: StateFlow<Float> = countDownTimeFlow.map { it / 60f }.stateIn(
+    val timerSweepAngle: StateFlow<Float> = countDownTimeFlow.map { it * (360f / maxTime) }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.Lazily,
         initialValue = 0f
