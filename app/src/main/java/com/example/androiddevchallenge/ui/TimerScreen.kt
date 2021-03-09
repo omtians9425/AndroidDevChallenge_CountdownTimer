d25d38549c71f8b7c5fa3f1fe2c70b644ca9e40d
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-10 01:21:56 +0900

    Show TopAppBar and Fab

--- app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
@@ -6,8 +6,9 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -19,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -40,34 +42,55 @@ fun TimerScreen() {
 
 @Composable
 fun StatelessTimerScreen(sweepAngle: Float, countDownTime: String) {
-    Box(contentAlignment = Alignment.Center) {
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            val arcSizePx = 300.dp.toPx()
-            translate(size.width / 2, size.height / 2) {
-                drawArc(
+    Scaffold(
+        topBar = {
+            TopAppBar(backgroundColor = Color.Transparent) {
+                Text(
+                    text = "Timer",
                     color = Color.White,
-                    startAngle = 0f,
-                    sweepAngle = 360f,
-                    useCenter = false,
-                    style = Stroke(width = 8.dp.toPx()),
-                    size = Size(arcSizePx, arcSizePx),
-                    topLeft = Offset(-(arcSizePx / 2), -(arcSizePx / 2))
-                )
-                drawArc(
-                    color = purple200,
-                    startAngle = -90f,
-                    sweepAngle = sweepAngle,
-                    useCenter = false,
-                    style = Stroke(width = 8.dp.toPx(), cap = StrokeCap.Round),
-                    size = Size(arcSizePx, arcSizePx),
-                    topLeft = Offset(-(arcSizePx / 2), -(arcSizePx / 2))
+                    textAlign = TextAlign.Center
                 )
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                backgroundColor = MaterialTheme.colors.primary,
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(Icons.Default.PlayArrow, contentDescription = "Start timer")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                val arcSizePx = 300.dp.toPx()
+                translate(size.width / 2, size.height / 2) {
+                    drawArc(
+                        color = Color.White,
+                        startAngle = 0f,
+                        sweepAngle = 360f,
+                        useCenter = false,
+                        style = Stroke(width = 8.dp.toPx()),
+                        size = Size(arcSizePx, arcSizePx),
+                        topLeft = Offset(-(arcSizePx / 2), -(arcSizePx / 2))
+                    )
+                    drawArc(
+                        color = purple200,
+                        startAngle = -90f,
+                        sweepAngle = sweepAngle,
+                        useCenter = false,
+                        style = Stroke(width = 8.dp.toPx(), cap = StrokeCap.Round),
+                        size = Size(arcSizePx, arcSizePx),
+                        topLeft = Offset(-(arcSizePx / 2), -(arcSizePx / 2))
+                    )
+                }
+            }
+            Text(
+                text = countDownTime,
+                color = MaterialTheme.colors.primary,
+                fontSize = 48.sp
+            )
         }
-        Text(
-            text = countDownTime,
-            color = MaterialTheme.colors.primary,
-            fontSize = 48.sp
-        )
     }
 }
