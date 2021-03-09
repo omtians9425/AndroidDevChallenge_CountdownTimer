312c86bfe16a2d9600b8dc18d403208882260a60
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-09 22:24:13 +0900

    Implement initial timer logic

--- app/src/main/java/com/example/androiddevchallenge/ui/TimerViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/TimerViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.androiddevchallenge.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.*
+
 
 class TimerViewModel : ViewModel() {
-    
+
+    private val countDownTimeFlow: Flow<Int> = flow {
+        var count = 0
+        while (true) {
+            emit(count)
+            count++
+            kotlinx.coroutines.delay(1000L)
+        }
+    }
+
+    val timerSweepAngle: StateFlow<Float> = countDownTimeFlow.map { it / 60f }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = 0f
+    )
 }
