fc998da8da27f6cead4f561fd5bc054af47f7a13
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-10 00:45:00 +0900

    Show background arc

--- app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/TimerScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.translate
@@ -44,6 +45,15 @@ fun StateLessTimerScreen(sweepAngle: Float, countDownTime: String) {
         Canvas(modifier = Modifier.fillMaxSize()) {
             val arcSizePx = 300.dp.toPx()
             translate(size.width / 2, size.height / 2) {
+                drawArc(
+                    color = Color.White,
+                    startAngle = 0f,
+                    sweepAngle = 360f,
+                    useCenter = false,
+                    style = Stroke(width = 8.dp.toPx()),
+                    size = Size(arcSizePx, arcSizePx),
+                    topLeft = Offset(-(arcSizePx / 2), -(arcSizePx / 2))
+                )
                 drawArc(
                     color = purple200,
                     startAngle = -90f,
